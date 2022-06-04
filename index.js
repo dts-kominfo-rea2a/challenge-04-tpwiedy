d5a23b1f2d5907d7440aa77488ca9b9d2d681788
tpwiedy <wiedy.pratama@gmail.com>
2022-06-04 10:14:45 +0700

    feat: Challenge 4 Done

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const sortDate = (arr) => {
+  let data = [];
+  for (let i = 0; i < arr.length; i++) {
+    // looping data
+    let loop = '';
+    loop += arr[i];
+    // menambahkan di array
+    data.push(Math.round(Date.parse(loop) / 1000));
+  }
+  let sortedData = data.sort().join('-');
+  return sortedData;
+};
+
+const createDate = (arr, arrI) => {
+  let date = 0;
+  if (arrI === undefined) {
+    return sortDate(arr);
+  } else {
+    date = Math.round(Date.parse(arr[arrI]) / 1000);
+    dateString = date.toString();
+    return dateString;
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
